dfb0b30d45ce6403eff49157c6722c0c34936492
Yuan Kuang <kuanyuan@meta.com>
2024-08-02 17:21:28 +0800

    centos clang issue.

--- pip_packages_auto_install.py
+++ pip_packages_auto_install.py
@@ -10,16 +10,27 @@ def create_venv(venv_name):
     subprocess.run(command, check=True)
 
 def activate_venv(venv_name):
-    command = f"source /Users/kuangyuan/Documents/columbus/%s/bin/activate && pip freeze" % venv_name
-    print(f"Running command: {' '.join(command)}")
+    command = "source ./%s/bin/activate && pip freeze" % venv_name
+    print(f"Running command: {command}")
     subprocess.run(command, shell=True, check=True)
 
-def install_packages(venv_name, dir):
-    for filename in os.listdir(dir):
+
+def install_packages(pip_dir):
+    pip_dir = '%s/%s' % (os.getcwd(), pip_dir)
+    os.chdir(pip_dir)
+    for filename in os.listdir(pip_dir):
         if '.whl' in filename:
-            command = ["./%s/bin/pip" % venv_name, "install", "./%s/%s" % (dir, filename)]
-            print(f"Running command: {' '.join(command)}")
-            subprocess.call(command, shell=True)
+            command = "pip install %s" % filename
+            print(f"Running command: {command}")
+            subprocess.run(command, shell=True, check=True)
+        elif '.tar.gz' in filename:
+            command = "tar -xzvf ./%s" % filename
+            print(f"Running command: {command}")
+            subprocess.run(command, shell=True, check=True)
+            os.chdir('%s/%s' % (pip_dir, filename.split(".tar.gz")[0]))
+            subprocess.run("python3 setup.py install", shell=True, check=True)
+            os.chdir(pip_dir)
+
 
 def activate_virtualenv_and_install(venv_path, packages_dir):
     # Determine the path to the activate script based on the operating system
@@ -47,7 +58,7 @@ def activate_virtualenv_and_install(venv_path, packages_dir):
             sys.exit(1)
 
 if __name__ == "__main__":
-    name = "columbus_work_station"
+    name = "columbus_work_station_venv"
     # create_venv(name)
     activate_venv(name)
-    install_packages(name, 'pip_packages')
+    install_packages('pip_packages')
